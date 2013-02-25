c886fffdf47892b2b561994f639f9f3ad395c925
Eric Shane <eshane@Mac110-Simic.local>
2013-02-25 08:56:19 -0500

    P2a set up + step 1

---
+++ FractionClass/ImmutableFraction.h
@@ -0,0 +1,13 @@
+//
+//  ImmutableFraction.h
+//  
+//
+//  Created by Eric Shane on 2/25/13.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ImmutableFraction : NSObject
+
+@end
---
+++ FractionClass/ImmutableFraction.m
@@ -0,0 +1,13 @@
+//
+//  ImmutableFraction.m
+//  
+//
+//  Created by Eric Shane on 2/25/13.
+//
+//
+
+#import "ImmutableFraction.h"
+
+@implementation ImmutableFraction
+
+@end
