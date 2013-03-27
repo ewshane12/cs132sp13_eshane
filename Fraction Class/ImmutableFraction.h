0671a0fba6b189da883bb93d3ee9120f9d2ca3f8
Eric Shane <eshane@Mac110-Simic.local>
2013-03-27 17:33:53 -0400

    P2a is being set up after some trouble, I am starting program 2 over again

---
+++ Fraction Class/ImmutableFraction.h
@@ -0,0 +1,13 @@
+//
+//  ImmutableFraction.h
+//  
+//
+//  Created by Eric Shane on 3/27/13.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ImmutableFraction : NSObject
+
+@end
---
+++ Fraction Class/ImmutableFraction.m
@@ -0,0 +1,13 @@
+//
+//  ImmutableFraction.m
+//  
+//
+//  Created by Eric Shane on 3/27/13.
+//
+//
+
+#import "ImmutableFraction.h"
+
+@implementation ImmutableFraction
+
+@end
