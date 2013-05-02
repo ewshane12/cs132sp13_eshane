0e5f3047042763b706f963618d2f0bf54f7c3638
Eric Shane <eshane@Mac110-Simic.local>
2013-05-02 17:10:57 -0400

    Committing part e

--- Fraction Class/Fraction Development 2/Fraction Development/main.m
+++ Fraction Class/Fraction Development 2/Fraction Development/main.m
@@ -7,8 +7,31 @@
 //
 
 #import <Cocoa/Cocoa.h>
+#import "WCSFraction.h"
+#import "WCSMutableFraction.h"
 
 int main(int argc, char *argv[])
 {
+    WCSFraction* oneThird;
+    WCSFraction* oneHalf;
+    WCSFraction* oneEigth;
+    WCSFraction* two;
+    WCSFraction* theMutant;
+    WCSFraction* theResult;
+    
+    oneThird = [[WCSFraction alloc] initWithNumerator:1 andDenominator:3];
+    oneHalf = [[WCSFraction alloc] initWithNumerator:1 andDenominator:2];
+    oneEigth = [[WCSFraction alloc] initWithNumerator:1 andDenominator:8];
+    two = [[WCSFraction alloc] initWithNumerator:2 andDenominator:1];
+    theMutant = [[WCSFraction alloc] initWithNumerator:5 andDenominator:8];
+    
+    theResult = [oneThird minus:oneHalf];
+    NSLog(@"%@",theResult);
+    
+    theResult = [[theMutant minus:oneEigth]multiplyBy:two];
+    NSLog(@"%@",theResult);
+    
+    
+    
     return NSApplicationMain(argc, (const char **)argv);
 }
